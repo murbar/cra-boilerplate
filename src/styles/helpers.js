8971da5b2c05a394534cf3985d685081e7b22af6
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-06 13:56:44 -0500

    Add some useful components

--- src/styles/helpers.js
+++ src/styles/helpers.js
@@ -1,21 +1,51 @@
 import { css } from 'styled-components';
 
-const sizes = {
-  desktop: 1050,
-  tablet: 760,
-  phone: 500
+export const DEVICE_SIZES = {
+  desktopXL: 1800,
+  desktop: 1200,
+  tablet: 900,
+  phone: 625
 };
 
-// Iterate through the sizes and create a media template
-export const media = Object.keys(sizes).reduce((acc, label) => {
-  acc[label] = (...args) => css`
-    @media (min-width: ${sizes[label] / 16}rem) {
+for (let px = 350; px <= 1800; px += 50) {
+  DEVICE_SIZES[`px${px}`] = px;
+}
+
+// Iterate DEVICE_SIZES and create a media template
+export const mediaQuery = {};
+mediaQuery.above = Object.keys(DEVICE_SIZES).reduce((obj, label) => {
+  obj[label] = (...args) => css`
+    @media (min-width: ${DEVICE_SIZES[label] / 16}em) {
       ${css(...args)}
     }
   `;
-  return acc;
+  return obj;
+}, {});
+mediaQuery.below = Object.keys(DEVICE_SIZES).reduce((obj, label) => {
+  obj[label] = (...args) => css`
+    @media (max-width: ${DEVICE_SIZES[label] / 16}em) {
+      ${css(...args)}
+    }
+  `;
+  return obj;
 }, {});
 
-export const addAlpha = (hsl, alpha) => {
-  return `${hsl.slice(0, -1)}, ${alpha})`;
+const parseHslString = hsl => {
+  const values = /hsla?\((\d+),\s*([\d.]+)%,\s*([\d.]+)%(?:,\s*)?([\d.]+)?\)/g.exec(hsl);
+  if (values) {
+    return values.slice(1).map(v => (v === undefined ? null : Number(v)));
+  } else {
+    console.warn('Invalid HSL value, returning black');
+    return [0, 0, 0, 0];
+  }
+};
+
+export const adjustHsl = (hsl, values) => {
+  values = { h: 0, s: 0, l: 0, a: 1, ...values };
+  const [h, s, l, a] = parseHslString(hsl);
+  const dH = h + values.h;
+  const dS = s + values.s;
+  const dL = l + values.l;
+  const dA = values.a;
+  return `hsla(${dH}, ${dS}%, ${dL}%, ${dA})`;
 };
