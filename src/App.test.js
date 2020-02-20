4dba01305bdfdcf6acd1ddd6a48f9bfcfd02c89e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-20 15:45:27 -0500

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from 'styles/theme';
+
+export const withContexts = (Component, props) => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...props} />
+    </ThemeProvider>
+  );
+};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(withContexts(App), div);
   ReactDOM.unmountComponentAtNode(div);
 });
